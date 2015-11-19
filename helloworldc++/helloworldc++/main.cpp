ef80bd58c06b6d20059ad0aff37d33d5f0989500
houlina02 <houlina02@meituan.com>
2015-11-19 16:51:46 +0800

    hello world c++

---
+++ helloworldc++/helloworldc++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloworldc++
+//
+//  Created by houlina02 on 15/11/19.
+//  Copyright (c) 2015年 houlina02. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
